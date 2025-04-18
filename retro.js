b4b0042f2739da5957800f3c5dc625da9edbe909
Hasibul Hasan <hasanchaion847@gmail.com>
2025-04-18 20:10:39 +0600

    latest section done

--- retro.js
+++ retro.js
@@ -3,24 +3,26 @@ const fetchData = async (inputValue) => {
     `https://openapi.programming-hero.com/api/retro-forum/posts`
   );
   const data = await ref.json();
+ 
   const posts = data.posts;
 
   displayPost(posts);
 };
 
 const displayPost = (posts) => {
+ 
 
   const apiContainer = document.getElementById("api-container");
   apiContainer.innerText=''
 
   posts.map((post) => {
-    // console.log(post);
+    
     const div = document.createElement("div");
     div.innerHTML = `
 
-                  <div class="avatar max-h-[80px] avatar-online">
+                  <div class="avatar check-online max-h-[80px] ">
                         <div class="w-[80px]  rounded-full p-2">
-                          <img src="${post.image}" />
+                          <img  src="${post.image}" />
                         </div>
                       </div>
 
@@ -32,7 +34,7 @@ const displayPost = (posts) => {
                             </p>
 
                         </div>
-                    <p class="color1 font-bold text-2xl">${post.title}</p>
+                    <p id="${post.id}" class="color1 font-bold text-2xl">${post.title}</p>
                     <p class="max-w-[569px] text-xl">${post.description}</p>
                     <hr>
                      <div class="flex items-center justify-between">
@@ -45,7 +47,7 @@ const displayPost = (posts) => {
                                 <p>${post.posted_time}</p>
                                 
                             </div>
-                            <button class="btn rounded-full ">
+                            <button  onClick="addInList(${post.id},${post.view_count})" class="btn rounded-full ">
                                 <img src="images/email 1.jpg" alt="">
                             </button> 
                         </div>
@@ -67,7 +69,13 @@ const displayPost = (posts) => {
       "gap-3",
       "my-4"
     );
-  });
+  
+
+   
+    
+});
+
+
 };
 const input=document.getElementById('inputField')
 const findInput=()=>{
@@ -91,6 +99,86 @@ const fetchByCategory=async(inputValue)=>{
    
 }
 
+ const addInList=(title,viewCount)=>{
+    const bd= document.getElementById(`${title}`) 
+    const text =bd.innerText
+    const checkBox=document.getElementById('check-box')
+    const div=document.createElement('div')
+    div.innerHTML=`
+                      <p class="w-[212px]">${text}</p>
+                        <div>
+                            <i class="fa-regular fa-eye"></i>
+                            <p>${viewCount}</p>
+    
+                        </div>`
+
+   
+    
+     
+    div.classList.add('flex','justify-between','bg-white','p-5','rounded-xl','my-5') 
+    checkBox.appendChild(div)
+   
+    
+   const countClick=document.getElementById('count-click')
+   const count=parseInt(countClick.innerText)
+   countClick.innerText=count+1;
+   
+    
+
+   
+ }
+  const fetchLatestPost= async()=>{
+       const ref=await fetch('https://openapi.programming-hero.com/api/retro-forum/latest-posts')
+       const data=await ref.json()
+       displayLatestPost(data)
+        
+
+  }
+
+  const displayLatestPost=(data)=>{
+     const latestPostContainer=document.getElementById('latestPostContainer')
+     
+    data.map((dt)=>{
+      console.log(dt)
+      const div= document.createElement('div')
+    div.innerHTML=`<figure>
+                      <img
+                        src="${dt.cover_image}"
+                        alt="Shoes" />
+                    </figure>
+                    <div class="card-body">
+                        <div class="flex items-center gap-2">
+                            <img class="w-5" src="images/Frame.png" alt="">
+                        <p>${dt.author.posted_date||'No Publish'}</p>
+                        </div>
+                      <h2 class="card-title">
+                      ${dt.title}
+                        <div class="badge badge-secondary">NEW</div>
+                      </h2>
+                      <p>${dt.description}</p>
+                      <div class="card-actions justify-start gap-9 ">
+                        <img class="max-w-[58px]" src="${dt.profile_image}" alt="">
+                        <div class="flex flex-col gap-3">
+                            <div class="badge badge-outline">${dt.author.name}</div>
+                            <div class="badge badge-outline">${dt.author.designation||'UNKNOWN'}</div>
+                        </div>
+                      </div>
+                    </div>`
+
+     div.classList.add('card','bg-base-100','w-96','shadow-sm')
+
+   latestPostContainer.appendChild(div) })
+
+  }
+
+  fetchLatestPost()
 
 
 fetchData();
+
+
+
+
+
+
+
